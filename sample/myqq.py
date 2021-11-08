17bbab735e9705bbb4b1157bcd09530e16f60912
mayn <415551921@qq.com>
2021-11-08 22:26:18 +0800

    初始化提交

--- sample/myqq.py
+++ sample/myqq.py
@@ -1,19 +1,16 @@
 import json
-import logging
-
+import logger
 import config
 from flask import Flask, request
+from gevent import pywsgi
 
 app = Flask(__name__)
 
 
 # 启动MyQQ的HTTP回调接口
 def start_listen():
-    app.run(
-        host=config.myqq['callback_ip'],
-        port=config.myqq['callback_port'],
-        debug=config.debug_mode
-    )
+    server = pywsgi.WSGIServer((config.myqq['callback_ip'], config.myqq['callback_port']), app, log=None)
+    server.serve_forever()
 
 
 # HTTP回调接口的定义
@@ -25,6 +22,6 @@ def deal_with_message():
     from_qq = data['MQ_fromQQ']
     if from_qq != config.qq['robot_self_qq'] and data['MQ_fromID'] in config.qq['available_qq_group']:
         msg = data['MQ_msg']
-        logging.info("%s：%s", from_qq, msg)
+        logger.info("%s说：%s", from_qq, msg)
         msg.split('+')  # MyQQ会自动把空格转为加号，所以这里要用+分隔
     return json.dumps({"status": 1})

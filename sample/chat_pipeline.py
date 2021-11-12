7c0d79b343616bb52fb87cac2fa5b2f9985596f8
FlyingLu <415551921@qq.com>
2021-11-12 13:28:49 +0800

    给chat_pipeline和message加注释

--- sample/chat_pipeline.py
+++ sample/chat_pipeline.py
@@ -6,12 +6,19 @@ from logger import logger
 
 
 class IChatPipeline(object, metaclass=abc.ABCMeta):
+    """这是消息处理器的父类，当你想要新增自己的消息处理器时，继承这个类即可。
+    最后，不要忘记在chat_pipeline_manager.py中的init_chat_pipeline函数里面加入你新增的这个消息处理器。"""
     @abc.abstractmethod
     def on_init(self):
+        """在程序开始，加载过程中，会执行这个函数。"""
         pass
 
     @abc.abstractmethod
     def execute(self, qq_group_number: str, qq: str, msg: str) -> str:
+        """每次收到QQ消息时会执行这个函数。
+        这个函数的返回值会传给下一个消息处理器，作为msg参数传入。
+        如果你不打算影响后续的消息处理器，在执行完自己的代码后，直接return msg即可。
+        如果你打算打断后续的消息处理器，那么return ‘’空字符串即可。"""
         pass
 
 
--- sample/message.py
+++ sample/message.py
@@ -5,16 +5,21 @@ import myqq
 
 
 class IMessageDispatcher(object, metaclass=abc.ABCMeta):
+    """这是聊天指令处理器的父类，当你想要新增自己的聊天指令处理器时，继承这个类即可。
+    最后，不要忘记在message_dispatcher.py中的init_message函数里面加入你新增的这个聊天指令处理器。"""
     def __init__(self, name: str, tips: str):
+        """name是群友输入聊天指令时，第一个空格前的内容。tips是查看帮助时显示的内容。"""
         self.name = name
         self.tips = tips
 
     @abc.abstractmethod
     def check_auth(self, qq: str) -> bool:
+        """如果他有权限执行这个指令，则返回True，否则返回False"""
         pass
 
     @abc.abstractmethod
     def execute(self, qq_group_number: str, qq: str, *args: str) -> None:
+        """args参数是一个str的list，也就是除开指令名（第一个空格前的部分）以外，剩下的所有内容按照空格进行拆分"""
         pass
 
 

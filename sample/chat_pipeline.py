781231675330841c9f9d93fc1012e5908b9a69b4
FlyingLu <415551921@qq.com>
2021-11-13 17:11:07 +0800

    mirai接入

--- sample/chat_pipeline.py
+++ sample/chat_pipeline.py
@@ -1,14 +1,15 @@
 import abc
 
+import config
 import message
 import message_dispatcher
-import config
 from logger import logger
 
 
 class IChatPipeline(object, metaclass=abc.ABCMeta):
     """这是消息处理器的父类，当你想要新增自己的消息处理器时，继承这个类即可。
     最后，不要忘记在chat_pipeline_manager.py中的init_chat_pipeline函数里面加入你新增的这个消息处理器。"""
+
     @abc.abstractmethod
     def on_init(self):
         """在程序开始，加载过程中，会执行这个函数。"""
@@ -30,9 +31,25 @@ class MessagePipeline(IChatPipeline):
     def on_init(self):
         message_dispatcher.init_message()
 
+    @staticmethod
+    def __is_at_message(msg_chain: list) -> True:
+        if len(msg_chain) < 2:
+            return False
+        contains_at = True
+        for idx in range(1, len(msg_chain)):
+            if msg_chain[idx]['type'] == 'At':
+                if msg_chain[idx]['target'] == config.qq['robot_self_qq']:
+                    contains_at = True
+            elif msg_chain[idx]['type'] == 'Plain':
+                if msg_chain[idx]['text'].strip() != '':
+                    return False
+            else:
+                return False
+        return contains_at
+
     def execute(self, qq_group_number: int, qq: int, msg_chain: list) -> str:
-        if len(msg_chain) == 2 and msg_chain[1]['type'] == 'At' and msg_chain[1]['target'] == config.qq['robot_self_qq']:
-            msg_chain[1] = {'type': 'Plain', 'text': '查看帮助'}
+        if self.__is_at_message(msg_chain):
+            msg_chain = [msg_chain[0], {'type': 'Plain', 'text': '查看帮助'}]
         if len(msg_chain) == 2 and msg_chain[1]['type'] == 'Plain':
             msg = msg_chain[1]['text']
             arr = msg.split(' ')

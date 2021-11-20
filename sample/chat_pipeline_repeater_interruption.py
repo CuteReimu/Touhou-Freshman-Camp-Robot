c9d22f19ad9631d6d2c10050b5c1d429c334b1f5
Phiomoe <2719509156@qq.com>
2021-11-20 14:45:29 +1100

    modified & fixed bugs

--- sample/chat_pipeline_repeater_interruption.py
+++ sample/chat_pipeline_repeater_interruption.py
@@ -1,32 +1,49 @@
 import chat_pipeline
 import mirai_bot
 from mirai_bot_chain import plain
-
-
-counter = 1
-last_msg = ""
+import config
 
 
 class RepeaterInterruptionPipeline(chat_pipeline.IChatPipeline):
     def on_init(self):
-        pass
+        self.counter = 1
+        self.last_msg_chain = []
 
     def execute(self, qq_group_number: int, qq: int, msg_chain: list) -> str:
-        global counter
-        global last_msg
-        
-        if len(msg_chain) == 2 and msg_chain[1]['type'] == 'Plain':
-            msg = msg_chain[1]['text']
-            if counter >= 4 and msg == "打断复读 ^-^":
-                mirai_bot.send_group_message(qq_group_number, [plain("(*/ω＼\*)")])
-                counter = 1
-            elif counter >= 4:
-                mirai_bot.send_group_message(qq_group_number, [plain("打断复读~~ (^-^)")])
-                counter = 1
-            if msg == last_msg:
-                counter += 1
+        if qq_group_number in config.repeater_interruption['qq_group']:
+            if self.counter >= config.repeater_interruption['allowance'] \
+            and msg_chain[1]['type'] == 'Plain' \
+            and msg_chain[1]['text'] == '打断复读 ^-^':
+                mirai_bot.send_group_message(qq_group_number, [plain('(*/ω＼\*)')])
+                self.counter = 0
+            elif self.counter >= config.repeater_interruption['allowance']:
+                mirai_bot.send_group_message(qq_group_number, [plain('打断复读~~ (^-^)')])
+                self.counter = 0
+            if self.__equals(msg_chain, self.last_msg_chain):
+                self.counter += 1
             else:
-                counter = 1
-            last_msg = msg
-                
-        return msg
+                self.counter = 1
+            self.last_msg_chain = msg_chain
+        return ' '
+
+    def __equals(msg_chain: list, last_msg_chain: list) -> bool:
+        if len(msg_chain) < 2 or len(msg_chain) != len(last_msg_chain):
+            return False
+
+        for msg_idx in range(1, len(msg_chain)):
+            if msg_chain[msg_idx]['type'] != last_msg_chain[msg_idx]['type']:
+                return False
+
+            if msg_chain[msg_idx]['type'] == 'Plain':
+                if msg_chain[msg_idx]['text'] == last_msg_chain[msg_idx]['text']:
+                    return True
+            elif msg_chain[msg_idx]['type'] == 'At':
+                if msg_chain[msg_idx]['target'] == last_msg_chain[msg_idx]['target']:
+                    return True
+            elif msg_chain[msg_idx]['type'] == 'Image':
+                if msg_chain[msg_idx]['imageId'] == last_msg_chain[msg_idx]['imageId']:
+                    return True
+                elif msg_chain[msg_idx]['url'] == last_msg_chain[msg_idx]['url']:
+                    return True
+
+        return False

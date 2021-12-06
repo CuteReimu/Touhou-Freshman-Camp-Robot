28ed5d9f8748da188ad5cc3e845544dafcfc89f9
mayn <415551921@qq.com>
2021-12-06 21:55:35 +0800

    增加了“更新公告”功能

--- sample/chat_pipeline_update.py
+++ sample/chat_pipeline_update.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 import chat_pipeline
@@ -11,10 +12,10 @@ class UpdatePipeline(chat_pipeline.IChatPipeline):
         self.__already_group_ids = set()
 
     def on_init(self):
-        p = Path('update.txt')
+        p = Path(os.path.join('..', 'update.txt'))
         if p.exists():
             self.__update_content = p.read_text('utf-8').strip()
-            p.rename('update.txt.bak')
+            p.rename(os.path.join('..', 'update.txt.bak'))
 
     def execute(self, qq_group_number: str, qq: str, msg: str) -> str:
         if len(self.__update_content) > 0 and qq_group_number in config.qq['available_qq_group']:

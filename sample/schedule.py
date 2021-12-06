48d64c430724b7385a6e0c7568eb8c1756888227
FlyingLu <415551921@qq.com>
2021-12-06 19:01:05 +0800

    优化schedule的多线程逻辑

--- sample/schedule.py
+++ sample/schedule.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 class Schedule:
     def __init__(self):
+        self.__lock = threading.Lock()
         self.__id = 0
         self.__scheduler = sched.scheduler(time.time, time.sleep)
         self.cache: dict[int, sched.Event] = {}
@@ -13,22 +14,41 @@ class Schedule:
     def run(self):
         threading.Thread(target=self.__scheduler.run, daemon=True).start()
 
-    def add(self, delay: int, action, argument=()) -> None:
-        event = self.__scheduler.enter(delay, 1, action, argument)
-        self.__id += 1
-        self.cache[self.__id] = event
+    def __action(self, action, action_id: int):
+        with self.__lock:
+            try:
+                self.cache.pop(action_id)
+            except KeyError:
+                pass
+        action()
 
-    def addabs(self, date_obj: datetime, action, argument=()) -> None:
-        event = self.__scheduler.enterabs(date_obj, 1, action, argument)
-        self.__id += 1
-        self.cache[self.__id] = event
+    def add(self, delay: int, action, argument=()) -> int:
+        with self.__lock:
+            self.__id += 1
+            if delay > 0:
+                event = self.__scheduler.enter(delay, 1, self.__action(action, self.__id), argument)
+                self.cache[self.__id] = event
+            else:
+                action()
+            return self.__id
+
+    def addabs(self, date_obj: datetime, action, argument=()) -> int:
+        with self.__lock:
+            self.__id += 1
+            if date_obj > datetime.now():
+                event = self.__scheduler.enterabs(date_obj, 1, action, argument)
+                self.cache[self.__id] = event
+            else:
+                action()
+            return self.__id
 
     def remove(self, idx: int) -> bool:
-        try:
-            schedule_data = self.cache.pop(idx)
-            self.__scheduler.cancel(schedule_data.event)
-        except KeyError:
-            return False
+        with self.__lock:
+            try:
+                schedule_data = self.cache.pop(idx)
+                self.__scheduler.cancel(schedule_data.event)
+            except KeyError:
+                return False
 
 
 schedule = Schedule()

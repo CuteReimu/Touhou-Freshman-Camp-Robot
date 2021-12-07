82e9088e81e31709ebeaf4461438c76cc0a93784
mayn <415551921@qq.com>
2021-12-07 22:24:26 +0800

    投票功能增加了字数限制和屏蔽词

--- sample/message_vote.py
+++ sample/message_vote.py
@@ -10,6 +10,7 @@ vote_qq_group = ''
 vote_content = ''
 vote_schedule_id = 0
 vote_cache: dict[str, str] = {}
+vote_forbidden_words: list[str] = []
 
 
 def get_vote_result(count: int = 0) -> str:
@@ -152,14 +153,91 @@ class DoVote(message.IMessageDispatcher):
 
     def execute(self, qq_group_number: str, qq: str, *args: str) -> None:
         if len(args) < 1:
+            myqq.send_group_message(qq_group_number, '指令格式如下：\n投票 投票答案')
             return
-        global vote_schedule_id, vote_cache
+        global vote_schedule_id, vote_cache, vote_forbidden_words
         if vote_schedule_id == 0:
             myqq.send_group_message(qq_group_number, '目前没有正在进行的投票')
             return
-        content = ' '.join(args)
+        content = ' '.join(args).strip()
+        if '\n' in content:
+            return
+        try:
+            if len(content.encode('gbk')) > 12:
+                return
+        except ValueError:
+            return
+        for forbidden_word in vote_forbidden_words:
+            if forbidden_word in content:
+                myqq.send_group_message(qq_group_number, '“%s”被禁止了' % forbidden_word)
+                return
         if qq in vote_cache:
             myqq.send_group_message(qq_group_number, '你将投票结果改为：' + content)
         else:
             myqq.send_group_message(qq_group_number, '你进行了投票：' + content)
         vote_cache[qq] = content
+
+
+class AddVoteForbiddenWords(message.IMessageDispatcher):
+    @property
+    def name(self) -> str:
+        return '增加投票屏蔽词'
+
+    @property
+    def tips(self) -> str:
+        global vote_schedule_id
+        if vote_schedule_id == 0:
+            return ''
+        return '增加投票屏蔽词 词语'
+
+    def check_auth(self, qq: str) -> bool:
+        return qq in message_admin.admin_cache
+
+    def execute(self, qq_group_number: str, qq: str, *args: str) -> None:
+        if len(args) < 1:
+            myqq.send_group_message(qq_group_number, '指令格式如下：\n增加投票屏蔽词 词语')
+            return
+        global vote_schedule_id, vote_cache, vote_forbidden_words
+        if vote_schedule_id == 0:
+            myqq.send_group_message(qq_group_number, '目前没有正在进行的投票')
+            return
+        content = ' '.join(args).strip()
+        vote_forbidden_words.append(content)
+        illegal_vote = []
+        for k, v in vote_cache.items():
+            if content in v:
+                illegal_vote.append(k)
+        for k in illegal_vote:
+            vote_cache.pop(k)
+        myqq.send_group_message(qq_group_number, '投票屏蔽词增加成功，现在的屏蔽词有：\n' + '\n'.join(vote_forbidden_words))
+
+
+class DelVoteForbiddenWords(message.IMessageDispatcher):
+    @property
+    def name(self) -> str:
+        return '删除投票屏蔽词'
+
+    @property
+    def tips(self) -> str:
+        global vote_schedule_id
+        if vote_schedule_id == 0:
+            return ''
+        return '删除投票屏蔽词 词语'
+
+    def check_auth(self, qq: str) -> bool:
+        return qq in message_admin.admin_cache
+
+    def execute(self, qq_group_number: str, qq: str, *args: str) -> None:
+        if len(args) < 1:
+            myqq.send_group_message(qq_group_number, '指令格式如下：\n删除投票屏蔽词 词语')
+            return
+        global vote_schedule_id, vote_forbidden_words
+        if vote_schedule_id == 0:
+            myqq.send_group_message(qq_group_number, '目前没有正在进行的投票')
+            return
+        content = ' '.join(args).strip()
+        try:
+            vote_forbidden_words.remove(content)
+            myqq.send_group_message(qq_group_number, '投票屏蔽词删除成功，现在的屏蔽词有：\n' + '\n'.join(vote_forbidden_words))
+        except ValueError:
+            myqq.send_group_message(qq_group_number, '没有这个屏蔽词')

13d14c36fc8f8c7939f47801b08ed34d170a37be
Death <546115696@qq.com>
2021-12-14 14:54:32 +0800

    增加推送视频功能

--- sample/main.py
+++ sample/main.py
@@ -1,8 +1,10 @@
 import logger
 import myqq
 from bilibili import bili
+from vedio_pusher import videoPusher
 
 if __name__ == '__main__':
     logger.init()
     bili.login()
     myqq.start_listen()
+    videoPusher.start()

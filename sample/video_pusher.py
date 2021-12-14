22115c6f9be5e9f9610fb62619cbd74853de7c55
Death <546115696@qq.com>
2021-12-14 16:56:56 +0800

    增加推送视频功能

--- sample/video_pusher.py
+++ sample/video_pusher.py
@@ -4,36 +4,39 @@ from schedule import schedule
 from bilibili import bili
 import config
 
-class newvideoPusher():
+class NewVideoPusher():
     def __init__(self, mid, delay: int = 600):
-        self.__mid = mid
+        self.__mid = int(mid)
         self.__delay = delay
+        if os.path.exists('../latestvideo.txt'):
+            with open('../latestvideo.txt', 'r') as f:
+                self.__lastestId = f.readline()
+        else:
+            self.__lastestId = ''
 
     def __get_new_video(self):
         video_list = bili.get_user_video(self.__mid)
         new_video_list = []
-        if os.path.exists('../latestvideo.txt'):
-            with open('../latestvideo.txt', 'r') as f:
-                lastestId = f.read()
-                if(lastestId != ''):
-                    i = 0
-                    while(video_list[i]['bvid'] != lastestId):
-                        new_video_list.append(video_list[i]['bvid'])
-                        i += 1
-        with open('../latestvideo.txt', 'w') as f:
-            f.write(video_list[0]['bvid'])
+        if(not self.__lastestId == ''):
+            i = 0
+            while(not video_list[i]['bvid'] == self.__lastestId):
+                new_video_list.append(video_list[i]['bvid'])
+                i += 1
+        if(not self.__lastestId == video_list[0]['bvid']):
+            self.__lastestId = video_list[0]['bvid']
+            with open('../latestvideo.txt', 'w') as f:
+                f.write(video_list[0]['bvid'])
         return new_video_list
 
     def __push_new_video(self):
         new_video_list = self.__get_new_video()
         print(new_video_list)
-        if (len(new_video_list) != 0):
+        if (not len(new_video_list) == 0):
             for i in new_video_list:
-                for j in config.qq['available_qq_group']:
-                    print(i,j)
+                for j in config.schedule['qq_group']:
                     chat_pipeline_manager.deal_with_msg(j, '', i)
     
     def start(self):
         schedule.add(self.__delay, self.__push_new_video)
 
-videoPusher = newvideoPusher(config.bilibili['mid'])
+videoPusher = NewVideoPusher(config.bilibili['mid'])

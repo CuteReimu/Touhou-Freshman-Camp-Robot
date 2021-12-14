56e9bf6cc6e0151c109ce220739c85f3db74a966
Death <546115696@qq.com>
2021-12-14 18:10:41 +0800

    增加视频推送功能

--- sample/video_pusher.py
+++ sample/video_pusher.py
@@ -5,9 +5,8 @@ from bilibili import bili
 import config
 
 class NewVideoPusher():
-    def __init__(self, mid, delay: int = 600):
+    def __init__(self, mid):
         self.__mid = int(mid)
-        self.__delay = delay
         if os.path.exists('../latestvideo.txt'):
             with open('../latestvideo.txt', 'r') as f:
                 self.__lastestId = f.readline()
@@ -17,12 +16,12 @@ class NewVideoPusher():
     def __get_new_video(self):
         video_list = bili.get_user_video(self.__mid)
         new_video_list = []
-        if(not self.__lastestId == ''):
+        if self.__lastestId != '':
             i = 0
-            while(not video_list[i]['bvid'] == self.__lastestId):
+            while video_list[i]['bvid'] != self.__lastestId:
                 new_video_list.append(video_list[i]['bvid'])
                 i += 1
-        if(not self.__lastestId == video_list[0]['bvid']):
+        if self.__lastestId != video_list[0]['bvid']:
             self.__lastestId = video_list[0]['bvid']
             with open('../latestvideo.txt', 'w') as f:
                 f.write(video_list[0]['bvid'])
@@ -31,12 +30,12 @@ class NewVideoPusher():
     def __push_new_video(self):
         new_video_list = self.__get_new_video()
         print(new_video_list)
-        if (not len(new_video_list) == 0):
+        if len(new_video_list) != 0:
             for i in new_video_list:
                 for j in config.schedule['qq_group']:
                     chat_pipeline_manager.deal_with_msg(j, '', i)
     
     def start(self):
-        schedule.add(self.__delay, self.__push_new_video)
+        schedule.add(config.schedule['video_push_delay'], self.__push_new_video)
 
 videoPusher = NewVideoPusher(config.bilibili['mid'])

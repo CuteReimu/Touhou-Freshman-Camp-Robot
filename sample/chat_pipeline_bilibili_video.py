d334d2f198d3d4bb65793e5bb11cd48e2b5e8a69
Phiomoe <47812475+365daysdreamer@users.noreply.github.com>
2021-12-20 13:50:04 +1100

    Update chat_pipeline_bilibili_video.py

--- sample/chat_pipeline_bilibili_video.py
+++ sample/chat_pipeline_bilibili_video.py
@@ -13,15 +13,15 @@ class BilibiliVideoPipeline(chat_pipeline.IChatPipeline):
         match_obj = re.match(r'^(?:https?://www\.bilibili\.com/video/)?av(\d{8})$', msg, re.I)
         resp = None
         if match_obj:
-            resp = bili.get_video_info(qq_group_number, qq, aid=int(match_obj.group(1)))
+            resp = bili.get_video_info(aid=int(match_obj.group(1)))
         else:
             match_obj = re.match(r'^(?:https?://(?:www\.bilibili\.com/video|b23\.tv)/)?bv([0-9A-Za-z]{10})$', msg, re.I)
             if match_obj:
-                resp = bili.get_video_info(qq_group_number, qq, bid=match_obj.group(1))
+                resp = bili.get_video_info(bid=match_obj.group(1))
             else:
                 match_obj = re.match(r'^https?://b23\.tv/[0-9A-Za-z]{7}$', msg, re.I)
                 if match_obj:
-                    resp = bili.get_video_info(qq_group_number, qq, short_url=match_obj.group(0))
+                    resp = bili.get_video_info(qq_group_number=qq_group_number, qq=qq, short_url=match_obj.group(0))
         if resp is not None:
             pic_url = '[pic=%s]' % resp['pic']
             url = 'https://www.bilibili.com/video/' + resp['bvid']

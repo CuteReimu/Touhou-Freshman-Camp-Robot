a83e0b6c23a9f9583099429732aee5ed0ecfe5ab
Phiomoe <47812475+365daysdreamer@users.noreply.github.com>
2021-12-28 13:25:14 +1100

    Fixed regular expression

--- src/chat_pipeline_bilibili_video.py
+++ src/chat_pipeline_bilibili_video.py
@@ -12,22 +12,36 @@ class BilibiliVideoPipeline(chat_pipeline.IChatPipeline):
     def execute(self, qq_group_number: str, qq: str, msg: str) -> str:
         if '[Reply' in msg:
             return msg
-        match_obj = re.search(r'(?<![A-Za-z0-9\W])[,\.，。]?(?:https?://www\.bilibili\.com/video/)?av(\d+)', msg, re.I)
+        match_obj = re.search(r'(?<![A-Za-z0-9\W])'
+                              r'[，。+\s]?'
+                              r'(?:https?://www\.bilibili\.com/video/)?'
+                              r'av(\d+)',
+                              msg, re.I)
         resp = None
         if match_obj:
             resp = bili.get_video_info(aid=int(match_obj.group(1)))
         else:
-            match_obj = re.search(r'(?<![A-Za-z0-9\W])[,\.，。]?(?:https?://(?:www\.bilibili\.com/video|b23\.tv)/)?bv([0-9A-Za-z]{10})', msg, re.I)
+            match_obj = re.search(r'(?<![A-Za-z0-9\W])'
+                                  r'[，。+\s]?'
+                                  r'(?:https?://(?:www\.bilibili\.com/video|b23\.tv)/)?'
+                                  r'bv([0-9A-Za-z]{10})',
+                                  msg, re.I)
             if match_obj:
                 resp = bili.get_video_info(bid=match_obj.group(1))
             else:
-                match_obj = re.search(r'(?<![A-Za-z0-9\W])(?:[,\.，。])?https?://b23\.tv/[0-9A-Za-z]{7}', msg, re.I)
+                match_obj = re.search(r'(?<![A-Za-z0-9\W])'
+                                      r'[，。+\s]?'
+                                      r'(https?://b23\.tv/[0-9A-Za-z]{7})',
+                                      msg, re.I)
                 if match_obj:
-                    bili.get_video_info(qq_group_number=qq_group_number, qq=qq, short_url=match_obj.group(0))
+                    bili.get_video_info(qq_group_number=qq_group_number,
+                                        qq=qq,
+                                        short_url=match_obj.group(1))
         if resp is not None:
             pic_url = '[pic=%s]' % resp['pic']
             url = 'https://www.bilibili.com/video/' + resp['bvid']
             up = resp['owner']['name']
-            ret = '{0}\n{1}\n{2}\nUP主：{3}\n视频简介：{4}'.format(pic_url, resp['title'], url, up, resp['desc'])
+            ret = '{0}\n{1}\n{2}\nUP主：{3}\n视频简介：{4}' \
+                .format(pic_url, resp['title'], url, up, resp['desc'])
             myqq.send_group_message(qq_group_number, ret)
         return msg
